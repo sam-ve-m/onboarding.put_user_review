c130b082b26a94c8b4177be5dbb81d239d3dadcb
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-08-03 11:16:51 -0300

    wip

--- func/src/repositories/oracle/base_repository.py
+++ func/src/repositories/oracle/base_repository.py
@@ -15,105 +15,50 @@ class OracleBaseRepository:
     infra = OracleInfrastructure
 
     @classmethod
-    async def query(cls, sql: str, value: str | int) -> list:
+    async def query(cls, sql: str, filters: List[str | int]) -> list:
         try:
             async with cls.infra.get_connection() as cursor:
-                await cursor.execute(sql, value=value)
+                await cursor.execute(sql, filters)
                 rows = await cursor.fetchall()
-
                 return rows
 
-        except cx_Oracle.DataError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except cx_Oracle.DataError as ex:
+            (error,) = ex.args
+            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {ex}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
 
-        except cx_Oracle.ProgrammingError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except cx_Oracle.ProgrammingError as ex:
+            (error,) = ex.args
+            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {ex}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
 
-        except cx_Oracle.InternalError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except cx_Oracle.InternalError as ex:
+            (error,) = ex.args
+            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {ex}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
 
-        except cx_Oracle.NotSupportedError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except cx_Oracle.NotSupportedError as ex:
+            (error,) = ex.args
+            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {ex}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
 
-        except cx_Oracle.DatabaseError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except cx_Oracle.DatabaseError as ex:
+            (error,) = ex.args
+            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {ex}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
 
-        except cx_Oracle.Error as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except cx_Oracle.Error as ex:
+            (error,) = ex.args
+            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Sql: {sql} - Oracle-ex: {ex}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
 
-        except Exception as e:
-            message = f"Exception: {e}. Oracle-Error-Base-Exception Sql: {sql}"
-            Gladsheim.error(error=e, message=message)
-            raise InternalServerError("common.process_issue")
-
-    @staticmethod
-    def _normalize_encode(rows: List[tuple]) -> List[tuple]:
-        new_rows = list()
-        for row in rows:
-            new_row = list()
-            for item in row:
-                if type(item) == str:
-                    item = item.encode().decode("utf-8", "strict")
-                new_row.append(item)
-            new_rows.append(tuple(new_row))
-        return new_rows
-
-    @classmethod
-    async def execute(cls, sql, values) -> None:
-        try:
-            async with cls.infra.get_connection() as cursor:
-                await cursor.execute(sql, values)
-
-        except cx_Oracle.DataError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Values: {values} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
-            raise InternalServerError("common.process_issue")
-
-        except cx_Oracle.ProgrammingError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Values: {values} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
-            raise InternalServerError("common.process_issue")
-
-        except cx_Oracle.InternalError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Values: {values} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
-            raise InternalServerError("common.process_issue")
-
-        except cx_Oracle.NotSupportedError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Values: {values} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
-            raise InternalServerError("common.process_issue")
-
-        except cx_Oracle.DatabaseError as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Values: {values} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
-            raise InternalServerError("common.process_issue")
-
-        except cx_Oracle.Error as e:
-            (error,) = e.args
-            message = f"Oracle-Error-Code: {error.code}. Oracle-Error-Message: {error.message} - Values: {values} - Oracle-ex: {e}"
-            Gladsheim.error(error=e, message=message)
+        except Exception as ex:
+            message = f"Exception: {ex=}. Oracle-Error-Base-Exception Sql: {sql=}"
+            Gladsheim.error(error=ex, message=message)
             raise InternalServerError("common.process_issue")
--- func/src/repositories/oracle/repository.py
+++ func/src/repositories/oracle/repository.py
@@ -12,19 +12,21 @@ class EnumerateRepository(OracleBaseRepository):
         sql = f"""
                 SELECT 1
     		    FROM USPIXDB001.SINCAD_EXTERNAL_PROFESSIONAL
-    		    WHERE CODE = :value
+    		    WHERE CODE = :code
             """
-        result = await cls.query(sql=sql, value=code)
+        result = await cls.query(sql=sql, filters=[code])
         return result
+
     @classmethod
-    async def get_city(cls, contry: str, state: str, id_city: float) -> list:
+    async def get_city(cls, country: str, state: str, id_city: float) -> list:
         sql = f"""
         SELECT 1
         FROM CORRWIN.TSCDXMUNICIPIO 
-        WHERE SIGL_PAIS = '{contry}' 
-        AND SIGL_ESTADO = '{state}' 
-        AND NUM_SEQ_MUNI = {id_city}"""
-        result = await cls.query(sql=sql, value=code)
+        WHERE SIGL_PAIS = :filter
+        AND SIGL_ESTADO = :filter
+        AND NUM_SEQ_MUNI = :filter
+        """
+        result = await cls.query(sql=sql, filters=[country, state, id_city])
         return result
 
     @classmethod
@@ -32,9 +34,9 @@ class EnumerateRepository(OracleBaseRepository):
         sql = f"""
             SELECT 1
             FROM CORRWIN.TSCPAIS
-            WHERE SG_PAIS = :value
+            WHERE SG_PAIS = :filter
         """
-        result = await cls.query(sql=sql, value=country_acronym)
+        result = await cls.query(sql=sql, filters=[country_acronym])
 
         return result
 
@@ -43,9 +45,9 @@ class EnumerateRepository(OracleBaseRepository):
         sql = f"""
             SELECT 1
             FROM USPIXDB001.SINCAD_EXTERNAL_MARITAL_STATUS
-            WHERE CODE = :value
+            WHERE CODE = :filter
         """
-        result = await cls.query(sql=sql, value=code)
+        result = await cls.query(sql=sql, filters=[code])
 
         return result
 
@@ -54,31 +56,29 @@ class EnumerateRepository(OracleBaseRepository):
         sql = f"""
             SELECT 1
             FROM USPIXDB001.SINCAD_EXTERNAL_NATIONALITY
-            WHERE CODE = :value
+            WHERE CODE = :filter
         """
-        result = await cls.query(sql=sql, value=code)
+        result = await cls.query(sql=sql, filters=[code])
 
         return result
 
 
-
-import asyncio
-
-
 if __name__ == '__main__':
 
+    import asyncio
+
     async def teste_repo():
-        result_country = await EnumerateRepository.get_country("BR")
+        result_country = await EnumerateRepository.get_country("BRA")
         result_ac = await EnumerateRepository.get_activity(105)
         result_na = await EnumerateRepository.get_nationality(1)
         result_ma = await EnumerateRepository.get_marital_status(1)
-
+        result_city = await EnumerateRepository.get_city(state="SP", country="BRA", id_city=5325)
+        print(result_city)
         print(result_ac)
         print(result_na)
         print(result_ma)
         print(result_country)
-        if not all([result_ma, result_na, result_ac, result_country]):
+        if not all([result_ma, result_na, result_ac, result_country, result_city]):
             raise ValueError("FALSE")
 
     asyncio.run(teste_repo())
-
